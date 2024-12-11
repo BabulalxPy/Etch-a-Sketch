b5ca2dc1242b6746039bb74a0429a2bd462bbb7a
BabulalxPy <babulalchaudhari8329@gmail.com>
2024-12-11 23:13:20 +0530

    create a basic and simple grid, still no interactive and dynamic. Gonna work on it soon. Arigatou!

---
+++ script.js
@@ -0,0 +1,69 @@
+const container = document.querySelector(".container");
+
+const row1 = document.createElement("div");
+row1.classList.add("row1");
+const row2 = document.createElement("div");
+row2.classList.add("row2");
+const row3 = document.createElement("div");
+row3.classList.add("row3");
+const row4 = document.createElement("div");
+row4.classList.add("row4");
+
+const div1 = document.createElement("div");
+const div2 = document.createElement("div");
+const div3 = document.createElement("div");
+const div4 = document.createElement("div");
+const div5 = document.createElement("div");
+const div6 = document.createElement("div");
+const div7 = document.createElement("div");
+const div8 = document.createElement("div");
+const div9 = document.createElement("div");
+const div10 = document.createElement("div");
+const div11 = document.createElement("div");
+const div12 = document.createElement("div");
+const div13 = document.createElement("div");
+const div14 = document.createElement("div");
+const div15 = document.createElement("div");
+const div16 = document.createElement("div");
+
+div1.textContent = "";
+div2.textContent = "";
+div3.textContent = "";
+div4.textContent = "";
+div5.textContent = "";
+div6.textContent = "";
+div7.textContent = "";
+div8.textContent = "";
+div9.textContent = "";
+div10.textContent = "";
+div11.textContent = "";
+div12.textContent = "";
+div13.textContent = "";
+div14.textContent = "";
+div15.textContent = "";
+div16.textContent = "";
+
+row1.appendChild(div1);
+row1.appendChild(div2);
+row1.appendChild(div3);
+row1.appendChild(div4);
+
+row2.appendChild(div5);
+row2.appendChild(div6);
+row2.appendChild(div7);
+row2.appendChild(div8);
+
+row3.appendChild(div9);
+row3.appendChild(div10);
+row3.appendChild(div11);
+row3.appendChild(div12);
+
+row4.appendChild(div13);
+row4.appendChild(div14);
+row4.appendChild(div15);
+row4.appendChild(div16);
+
+container.appendChild(row1);
+container.appendChild(row2);
+container.appendChild(row3);
+container.appendChild(row4);
